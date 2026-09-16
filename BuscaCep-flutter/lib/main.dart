154efd3105191b34975ad4abab3bbe105785d118
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-09-16 20:22:30 -0300

    :lipstick:style: Theme application in the main file

--- BuscaCep-flutter/lib/main.dart
+++ BuscaCep-flutter/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:busca_cep/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  
-  
   runApp(MaterialApp(
     // home: ,
     debugShowCheckedModeBanner: false,
-  ));
+    theme: AppTheme.lightTheme,
+    darkTheme: AppTheme.darkTheme,
+    themeMode: ThemeMode.system, //How the theme application is defined
+  )); 
 }
