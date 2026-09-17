5f3ae464c5ad9e39e5cc158ac84b6ec766804078
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-09-17 16:14:19 -0300

    :sparkles:feat: HomeScreen's route

--- BuscaCep-flutter/lib/main.dart
+++ BuscaCep-flutter/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:busca_cep/ui/home_screen.dart';
 import 'package:busca_cep/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    // home: ,
+    home: HomeScreen(),
     debugShowCheckedModeBanner: false,
     theme: AppTheme.lightTheme,
     darkTheme: AppTheme.darkTheme,
